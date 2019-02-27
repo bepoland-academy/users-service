58cd44922c784855eb45681a1d0ec0ba4249d33c
wkrzyzanowski <wiktor.krzyzanowski1@gmail.com>
2019-02-27 12:17:03 +0100

    Setup JUnit5 and Mockito

---
+++ src/test/pl/betse/beonetime/SimpleTest.java
@@ -0,0 +1,16 @@
+package pl.betse.beonetime;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SimpleTest {
+
+
+    @Test
+    void simpleTest1() {
+        assertEquals(1, 1);
+    }
+
+}
